11436c64ba014a52b6984b0b481e68352c43e4e7
Jpatki27 <106022738+Jpatki27@users.noreply.github.com>
2022-07-01 18:38:07 -0400

    Delete JavaBasics/src/inheritance directory

--- JavaBasics/src/inheritance/D.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class D extends E {
-	
-    public void date () {
-    	System.out.println("It is the 29th of May, 2022");
-    }
-//Hello
-}
--- JavaBasics/src/inheritance/E.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class E {
-	
-	public void nextYear() {
-		System.out.println("Next year will be 2023");
-	}
-
-}
